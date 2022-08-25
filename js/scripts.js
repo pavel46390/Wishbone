c188f03f467f29c4ab4932b3eb7e100355c5af10
pablo <pavel.petukhov.003@gmail.com>
2022-08-25 11:54:48 +0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         autoplay: true,
-        autoplayTimeout: 5000,
+        autoplayTimeout: 6000,
         smartSpeed: 800,
         margin:0,
         nav:true,
